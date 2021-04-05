a60371673b947ac0066d294a7392350b4a76cb5e
renukaseepersad <71949929+renukaseepersad@users.noreply.github.com>
2021-04-04 23:42:30 -0400

    adding textviews for name and email

--- app/src/main/java/com/example/finalproject/Profile.java
+++ app/src/main/java/com/example/finalproject/Profile.java
@@ -3,12 +3,23 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    TextView name, email;
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logout = findViewById(R.id.logout);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+
+
     }
 }
